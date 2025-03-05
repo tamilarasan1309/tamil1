b560f0a9fefc65667b9ef1855acfb2ff46eef42c
tamilarasan1309 <tamilaharasan@gmail.com>
2025-03-05 17:54:58 +0530

    Created Variable Program

---
+++ Variables.dart
@@ -0,0 +1,8 @@
+void main(){
+    // This Program is for Variables in Dart
+  int Age = 18;
+  string Name =("Tamilarasan");
+  var Dept = ("AI&DS");
+  var Phone_No = 8610308140;
+  print(Name\nAge\nDept\nPhone_No);
+}
